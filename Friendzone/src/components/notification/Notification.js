31a761ccec27d738811c25c4b30024bf52eea7ba
nguyendung300694 <nguyendung300694@gmail.com>
2017-11-25 00:07:34 +0700

    Update2411

--- Friendzone/src/components/notification/Notification.js
+++ Friendzone/src/components/notification/Notification.js
@@ -6,7 +6,7 @@ import { Languages } from '../../content/languages/Languages';
 import Image1 from '../../content/images/LoginBackGround1.png';
 import Image2 from '../../content/images/LoginBackGround2.jpg';
 import Image3 from '../../content/images/LoginBackGround3.jpg';
-
+import Color from '../../content/color/Color';
 
 const Item =
     (<View
@@ -26,19 +26,21 @@ const Item =
             <Text numberOfLines={1} ellipsizeMode={'tail'} style={{ fontSize: 10 }}>20:08</Text>
         </View>
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Image source={Image3} style={{ width: 36, height: 36, borderRadius: 18 }} />
+            <Image source={Image3} style={{ width: 36, height: 36 }} />
         </View>
     </View>);
 
 class Notification extends Component {
     render() {
         const { lang } = this.props;
+        const { white } = Color;
         return (
             <View>
                 <Text style={{ display: 'none' }}>
                     {lang}
                 </Text>
                 <FlatList
+                    style={{ backgroundColor: white }}
                     data={[{ key: 'a' }, { key: 'b' }, { key: 'c' },
                     { key: 'd' }, { key: 'e' }, { key: 'f' }, { key: 'g' },
                     { key: 'h' }, { key: 'k' }, { key: 'l' },
